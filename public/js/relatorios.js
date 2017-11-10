88355906b609f07213a96c5b7d990ad6b27c9e52
Mateus <seraomateus@hotmail.com>
2017-11-10 09:11:49 -0200

    continuando processo

--- public/js/relatorios.js
+++ public/js/relatorios.js
@@ -1,5 +1,21 @@
+function deletaLinhas(tabela, linha, novaLinha){
+    linha = tabela.rows.length;
+    while(linha > 0){
+        tabela.deleteRow(0);
+        linha = tabela.rows.length;
+    }
+    novaLinha = tabela.insertRow(0);
+    
+    return novaLinha;
+}
+
 function searchReport(report) {
     
+    var tabela, linha, coluna, novaLinha, corpo;
+    tabela = document.getElementById("table");
+    
+    linha = tabela.rows.length;
+     
     switch (report){
 
         case "filtro":
@@ -8,40 +24,49 @@ function searchReport(report) {
 
         case "agentes":
             document.getElementById("search").placeholder = "Nome do agente";
-
-            var tabela = document.getElementById("table");
-            var linha = tabela.rows.length;
-            var coluna = tabela.rows[linha-1].cells.length;
-            var novaLinha = tabela.insertRow(linha);
+    
+            novaLinha = deletaLinhas(tabela, linha, novaLinha);
+            console.log("ANTES:")
             
             var elementos = ["nome","sexo","CPF","Celular","senha"];
 
+            console.log("VAI: "+tabela.rows.length);
             for(var i = 0; i< 5; i++){
             var novaColuna = novaLinha.insertCell(i);
                novaColuna.innerHTML = elementos[i];
             };
-            var corpo = document.createElement("tbody");
+
+            corpo = document.createElement("tbody");
             corpo.setAttribute("id", "tbAgentes");
             tabela.appendChild(corpo);
+            
             
         break;
 
         case "rotaTodas":
             document.getElementById("search").placeholder = "Todas as Rotas";
 
-            /*$.ajax({
+           novaLinha = deletaLinhas(tabela, linha, novaLinha);
+           //novaLinha = tabela.insertRow(0);
+            
+            var elementos = ["cod","nome","Agente","status","data"];
+
+            $.ajax({
                 method: "GET",
                 url: "/todasRotas"
             })
-            .done(agente =>{
-                
-               $("#cod").val(agente[0].cod);
-               $("#nome").val(agente[0].nome);
-               $("#sexo").val(agente[0].sexo);
-               $("#cpf").val(agente[0].cpf);
-               $("#cel").val(agente[0].cel);
-               $("#senha").val(agente[0].senha);
-            });*/
+            .done(rota =>{
+            
+                for(var i = 0; i< 5; i++){
+                    var novaColuna = novaLinha.insertCell(i);
+                       novaColuna.innerHTML = elementos[i];
+                    };
+                    corpo = document.createElement("tbody");
+                    corpo.setAttribute("id", "tbAllRoutes");
+                    tabela.appendChild(corpo);
+                    addTableAllRoute(rota, tabela, linha, novaLinha);
+               
+            });
         break;
 
         case "rotaAbertas":
@@ -80,3 +105,78 @@ function searchReport(report) {
     }
 	
 };
+
+function addTableAllRoute(todas, tabela, linha, novaLinha){
+    var novaColuna;
+   //Arrumar se nulo.
+    if(todas.length === 0){
+        newRow = $("<tr>");
+        columns = "";
+        columns += '<td colspan="7" class="text-center danger">Nenhum dado encontrado.</td>'
+        newRow.append(columns);
+        $("#tbAllRoutes").append(newRow);
+    }else{
+        for (var i = 0; i<todas.length; i++){
+
+            novaLinha = tabela.insertRow(tabela.rows.length);
+           
+            novaColuna = novaLinha.insertCell(0);
+            novaColuna.innerHTML = todas[i].cod;
+           
+            novaColuna = novaLinha.insertCell(1);
+            novaColuna.innerHTML = todas[i].nome;
+            
+            novaColuna = novaLinha.insertCell(2);
+            novaColuna.innerHTML = todas[i].codAgente;
+
+            novaColuna = novaLinha.insertCell(3);
+            novaColuna.innerHTML = todas[i].status;
+
+            var novaColuna = novaLinha.insertCell(4);
+            
+
+            if(todas[i].data == null)
+                novaColuna.innerHTML = "";
+            else{
+                var data = new Date(todas[i].data);
+                novaColuna.innerHTML = data.toLocaleDateString();
+            }
+        }
+    }
+    
+    return false;
+};
+/*
+function addTableAllRoute(todas,tabela, linha, novaLinha){
+    var newRow, columns;
+    
+    if(todas.length === 0){
+        newRow = $("<tr>");
+        columns = "";
+        columns += '<td colspan="7" class="text-center danger">Nenhum dado encontrado.</td>'
+        newRow.append(columns);
+        $("#tbAllRoutes").append(newRow);
+    }else{
+        for (var i in todas){
+            
+            newRow = $("<tr>");
+            columns = "";
+            
+            columns += "<td>" + todas[i].cod + "</td>";
+            columns += "<td>" + todas[i].nome + "</td>";
+            columns += "<td>" + todas[i].codAgente + "</td>";
+            columns += "<td>" + todas[i].status + "</td>";
+            if(todas[i].data == null)
+                columns += "<td></td>";
+            else{
+                var data = new Date(todas[i].data);
+                columns += "<td>" + data.toLocaleDateString() + "</td>";
+            }
+            
+            newRow.append(columns);
+            $("#tbAllRoutes").append(newRow);
+        }
+    }
+    
+    return false;
+};*/
